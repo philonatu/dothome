425ec77b60f6be3c0413f4961e3cdeab0b5cdcd9
philonatu <philonatu@gmail.com>
2021-08-22 01:39:26 +0900

    Add Text File [index.php]

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+  include "container/lib01.php";
+?>
+<!doctype html>
+<html lang="ko">
+  <head>
+    <?php
+      echo file_GET_contents("container/head.php");
+    ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="header">
+        <?php
+          echo file_GET_contents("container/header.php");
+        ?>
+      </div>
+
+      <div class="main">
+        <?php
+          echo file_GET_contents("container/main.php");
+        ?>
+      </div>
+      <div class="backtotop" style="text-align:center;">
+        <a href="#top">화면 맨 위로</a>
+      </div>
+
+      <div class="footer">
+        <?php
+          echo file_GET_contents("container/footer.php");
+        ?>
+      </div>
+    </div>
+  </body>
+
+</html>
